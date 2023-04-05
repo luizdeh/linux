327b358d5c9e76e12cb161719b98347ae87811e1
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-04-05 16:32:58 -0300

    feat: new colorscheme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,7 +1,7 @@
 return {
   {
     'sainnhe/everforest',
-    enabled = true,
+    enabled = false,
     name = 'everforest',
     lazy = false,
     priority = 1000,
@@ -13,6 +13,9 @@ return {
       -- vim.cmd [[highlight IndentBlankLineContextStart guisp=#909495 gui=underline]]
       vim.cmd [[highlight IndentBlankLineChar guifg=#484a4a gui=nocombine]]
       vim.cmd [[highlight IndentBlankLineContextChar guifg=#909495 gui=nocombine]]
+      vim.cmd [[highlight NvimTreeCursorLine guifg=#a7c080 gui=bold]]
+      vim.cmd [[highlight NvimTreeOpenedFile gui=italic]]
+      vim.cmd [[highlight FidgetTask guifg=#5f6f67]]
     end,
   },
   {
@@ -31,6 +34,8 @@ return {
       -- vim.cmd [[highlight IndentBlankLineContextStart guisp=#909495 gui=underline]]
       vim.cmd [[highlight IndentBlankLineChar guifg=#484a4a gui=nocombine]]
       vim.cmd [[highlight IndentBlankLineContextChar guifg=#909495 gui=nocombine]]
+      vim.cmd [[highlight NvimTreeOpenedFile guifg=#a7d933 gui=italic]]
+      vim.cmd [[highlight NvimTreeCursorLine guifg=#a7c080 gui=bold]]
     end,
   },
   {
@@ -46,6 +51,65 @@ return {
       -- vim.cmd [[highlight IndentBlankLineContextStart guisp=#909495 gui=underline]]
       vim.cmd [[highlight IndentBlankLineChar guifg=#484a4a gui=nocombine]]
       vim.cmd [[highlight IndentBlankLineContextChar guifg=#909495 gui=nocombine]]
+      vim.cmd [[highlight NvimTreeOpenedFile guifg=#a7d933 gui=italic]]
+      vim.cmd [[highlight NvimTreeCursorLine guifg=#a7c080 gui=bold]]
+      vim.cmd [[highlight FidgetTask guifg=#5f6f67]]
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true,
+        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = { "italic" },
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+        },
+      })
+      vim.cmd('colorscheme catppuccin')
+      vim.cmd [[highlight IndentBlankLineChar guifg=#484a4a gui=nocombine]]
+      vim.cmd [[highlight IndentBlankLineContextChar guifg=#909495 gui=nocombine]]
+      vim.cmd [[highlight NvimTreeOpenedFile guifg=#8beb60 gui=bold]]
+      vim.cmd [[highlight NvimTreeCursorLine guifg=#ffb732 gui=bold]]
+      vim.cmd [[highlight FidgetTask guifg=#5f6f67]]
     end,
   },
 }
