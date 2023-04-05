fc308eff4e720ca73f3338a3540a5be512f7d2e1
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-04-05 16:32:29 -0300

    feat(harpoon): can use up to <leader>9 for marked files

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   'theprimeagen/harpoon',
   keys = {
     { "<leader>a",
@@ -12,39 +12,29 @@ local M = {
       end,
     },
   },
+  config = function()
+    local harpoon = require('harpoon')
+
+    harpoon.setup()
+
+    require('telescope').load_extension('harpoon')
+
+    local ui = require('harpoon.ui')
+
+    vim.keymap.set('n', '<A-p>', function() ui.nav_prev() end)
+    vim.keymap.set('n', '<A-n>', function() ui.nav_next() end)
+
+    local nmap = function(tbl)
+      vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
+    end
+
+    for i = 1, 9 do
+      nmap {
+        string.format('<leader>%s', i),
+        function()
+          require('harpoon.ui').nav_file(i)
+        end,
+      }
+    end
+  end
 }
-
-M.opts = function()
-  global_settings = {
-    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-    save_on_toggle = false,
-    -- saves the harpoon file upon every change. disabling is unrecommended.
-    save_on_change = true,
-    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-    enter_on_sendcmd = false,
-    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-    tmux_autoclose_windows = false,
-    -- filetypes that you want to prevent from adding to the harpoon list menu.
-    excluded_filetypes = { "harpoon" },
-    -- set marks specific to each git branch inside git repository
-    mark_branch = false,
-  }
-end
-
-M.config = function()
-  local harpoon = require('harpoon')
-
-  harpoon.setup(opts)
-  require('telescope').load_extension('harpoon')
-
-  local ui = require('harpoon.ui')
-
-  vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
-  vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
-  vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
-  vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
-  vim.keymap.set('n', '<A-p>', function() ui.nav_prev() end)
-  vim.keymap.set('n', '<A-n>', function() ui.nav_next() end)
-end
-
-return M
