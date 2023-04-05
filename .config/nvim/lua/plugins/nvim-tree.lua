975a77dce54e99168a8e916dd5391c785a1515cc
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-04-05 16:42:31 -0300

    feat(nvim-tree): enabled git, git glyphs and highlighted open files

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -7,17 +7,43 @@ return {
     require('nvim-tree').setup({
       view = {
         width = 35,
+        signcolumn = "yes",
       },
       filters = {
         custom = { "^.git$", "node_modules" }
       },
       git = {
-        enable = false,
+        enable = true,
         ignore = true,
       },
       hijack_unnamed_buffer_when_opening = false,
       hijack_cursor = true,
       update_cwd = true,
+      renderer = {
+        highlight_opened_files = "all",
+        icons = {
+          git_placement = "signcolumn",
+          padding = " ",
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+            modified = true,
+          },
+          glyphs = {
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      }
     })
   end,
 }
